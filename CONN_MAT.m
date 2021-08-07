471c8b4b17755a664a9a88168cf38089cc51f6f1
Sivakumar Karunakaran <sivakumark006@gmail.com>
2021-08-07 11:27:23 +0530

    Adjacency matrix for random connected simple graph
    Construct the symmetric adjacency matrix corresponding to the connected graph on given n vertices and e edges.

--- CONN_MAT.m
+++ CONN_MAT.m
@@ -1,5 +1,4 @@
 function A=CONN_MAT(n,e)
-nargin
 % Example A=CONN_MAT(10,15), n>0 (n is integer), e<=nchoose(n,2)
 % This algorithm used to construct the symmetric adjacency matrix corresponding to
 % the connected graph on n vertices and e edges. This code construct the
