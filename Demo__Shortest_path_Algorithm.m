1c5974fef9a30c2518b8221d51ac9665fada5ff5
Sivakumar Karunakaran <sivakumark006@gmail.com>
2021-08-07 11:35:31 +0530

    Demo for this work
    This algorithm is a demo for shortest path algorithm

--- Demo__Shortest_path_Algorithm.m
+++ Demo__Shortest_path_Algorithm.m
@@ -3,3 +3,4 @@ A=RAND_MAT(20,25); % Random symmetric adjacency matrix construted  on 20 vertice
 [ k, SPG, n]=Sequence_Of_Powers_Of_AL(A); % Constructed the sequence of power of neighbourhood matrices 
 i=10; j=20; % i is initial vertex and j is terminal vertex
 [DS_P, S_P]=Shortest_path_AL(i,j,k,SPG) % Constructed the shortest path between i and j
+
